3c2cfd10371571c5a394fea31ca099f3f97be6cd
Danver Kanyemba <danverkanyemba33@gmail.com>
2021-07-10 22:44:29 +0200

    added new columns

--- database/migrations/2021_05_23_063335_create_transport_requests_table.php
+++ database/migrations/2021_05_23_063335_create_transport_requests_table.php
@@ -16,16 +16,16 @@ class CreateTransportRequestsTable extends Migration
         Schema::create('transport_requests', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->boolean('seen')->default(0);
+            $table->boolean('approved_by_hod')->default(0);
+            $table->boolean('approved_by_transport')->default(0);
             $table->integer('user_id')->index();
-            $table->string('name');
+            $table->string('names_of_people');
             $table->integer('no_of_People');
             $table->string('destination');
             $table->date('departure_date');
             $table->string('departure_time');            
             $table->date('return_date');
             $table->string('return_time');
-            $table->boolean('recommended_by_hod')->default(0);
 //
         });
     }
